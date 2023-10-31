b174b84fc2f02fcc6905f07705b201eb5766c2a9
Rutetid <50543186+Rutetid@users.noreply.github.com>
2023-10-31 20:13:15 +0530

    Update RockPaperScissor.cpp

--- RockPaperScissor.cpp
+++ RockPaperScissor.cpp
@@ -1,1 +1,95 @@
+#include <iostream>
 
+enum choice {
+  rock ,
+  paper ,
+  scissor ,
+  quit
+};
+
+int main() {
+
+  using namespace std;
+  
+  int user = 0;
+  int AI = 0;
+  int tie = 0;
+  int input;
+  cout << "Welcome to Rock, Paper, Scissors"<<endl;
+  cout<< "Type rock/paper/scissor or quit to exit"<<endl;
+
+  int AiChoice = (rand() % 3);
+   cin >> input;
+  switch(input){
+    case rock:
+    if (AiChoice == 1){
+      cout<< "You chose rock"<<endl;
+      cout<< "AI chose paper"<<endl;
+      cout<< "AI wins"<<endl;
+      AI++;
+    }
+
+    else if (AiChoice == 2){
+      cout<< "You chose rock"<<endl;
+      cout<< "AI chose scissor"<<endl;
+      cout<< "you win"<<endl;
+      user++;
+    }
+
+    else if (AiChoice == 0){
+      cout<< "You chose rock"<<endl;
+      cout<< "AI chose rock"<<endl;
+      cout<< "you tied"<<endl;
+      tie++;
+    }
+    break;
+
+    case paper:
+    if (AiChoice == 1){
+      cout<< "You chose paper"<<endl;
+      cout<< "AI chose paper"<<endl;
+      cout<< "you tied"<<endl;
+      tie++;
+    }
+
+    else if (AiChoice == 2){
+      cout<< "You chose paper"<<endl;
+      cout<< "AI chose scissor"<<endl;
+      cout<< "AI wins"<<endl;
+      AI++;
+    }
+
+    else if (AiChoice == 0){
+      cout<< "You chose paper"<<endl;
+      cout<< "AI chose rock"<<endl;
+      cout<< "you win"<<endl;
+      user++;
+    }
+    break;
+
+    case scissor:
+    if (AiChoice == 1){
+      cout<< "You chose scissor"<<endl;
+      cout<< "AI chose paper"<<endl;
+      cout<< "you win"<<endl;
+      user++;
+    }
+
+    else if (AiChoice == 2){
+      cout<< "You chose scissor"<<endl;
+      cout<< "AI chose scissor"<<endl;
+      cout<< "you tie"<<endl;
+      tie++;
+    }
+
+    else if (AiChoice == 0){
+      cout<< "You chose scissor"<<endl;
+      cout<< "AI chose rock"<<endl;
+      cout<< "AI wins"<<endl;
+      AI++;
+    }
+    break;
+
+    
+}
+}
